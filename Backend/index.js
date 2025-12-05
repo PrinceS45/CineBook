54f837e9be4a882137f40e85ef8ca5e32692a7e5
Prince Singh <153863740+PrinceS45@users.noreply.github.com>
2025-12-05 09:09:06 +0530

    json parsing

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,7 @@ const app = express() ;
 
 
 app.use(express.json()) ; 
+app.use(express.urlencoded({extended : true})) ; 
 
 app.get("/health" , (req , res) => {
     return res.status(200).json({
